60ae12c6a79a34378a1f5ee2a6cafde2b8c25af3
ANISUL-ISLAM-ONI <aminjoy82@gmail.com>
2022-11-24 17:42:16 +0600

    dep

--- dist/quranfiles/transliterations/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var en_transliteration = require('./en.transliteration.json');
-var tr_transliteration = require('./tr.transliteration.json');
-module.exports = {
-  'English : English Transliteration': en_transliteration,
-  'Turkish : Muhammet Abay': tr_transliteration
-};
