ebc1042133df54e2ec0dd2a5f7c93795a28aac87
ANISUL-ISLAM-ONI <aminjoy82@gmail.com>
2022-11-25 17:31:28 +0600

    express middleware added

--- src/index.js
+++ src/index.js
@@ -1,29 +1,82 @@
+// require('dotenv').config();
+// const { ApolloServer } = require('apollo-server');
+// const { ApolloServerPluginLandingPageLocalDefault } = require('apollo-server-core');
+// const mongoose = require('mongoose');
+// const typeDefs = require('./graphql/typeDefs');
+// const resolvers = require('./graphql/resolvers');
+// // const dataSources = require('./graphql/datasources');
+
+// const server = new ApolloServer({
+//     typeDefs,
+//     resolvers,
+//     // dataSources: () => {
+//     //     return {
+//     //         ...dataSources
+//     //     }
+//     // },
+//     csrfPrevention: true,
+//     cache: 'bounded',
+//     plugins: [
+//         ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+//     ],
+// });
+
+// mongoose.connect(process.env.DATABASE, {useUnifiedTopology: true, useNewUrlParser: true}).then(() => {
+//     console.log("DB connected successfully");
+//     return server.listen({hostname: "0.0.0.0", port: process.env.PORT || 8000});
+// }).then(({url}) => {
+//     console.log(`🚀 Server running at ${url}`);
+// });
+
+
 require('dotenv').config();
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const { ApolloServerPluginDrainHttpServer } = require('@apollo/server/plugin/drainHttpServer');
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const { ApolloServerPluginLandingPageLocalDefault } = require('apollo-server-core');
 const mongoose = require('mongoose');
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
-// const dataSources = require('./graphql/datasources');
 
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    // dataSources: () => {
-    //     return {
-    //         ...dataSources
-    //     }
-    // },
-    csrfPrevention: true,
-    cache: 'bounded',
-    plugins: [
-        ApolloServerPluginLandingPageLocalDefault({ embed: true }),
-    ],
-});
+async function startApolloServer(){
+    const app = express();
+    const httpServer = http.createServer(app);
+    let server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        csrfPrevention: true,
+        cache: 'bounded',
+        plugins: [
+            ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+            ApolloServerPluginDrainHttpServer({ httpServer }),
+        ],
+    });
+    mongoose.connect(process.env.DATABASE, {useUnifiedTopology: true, useNewUrlParser: true}).then(() => {
+        console.log("DB connected successfully");
+    });
+    await server.start();
 
-mongoose.connect(process.env.DATABASE, {useUnifiedTopology: true, useNewUrlParser: true}).then(() => {
-    console.log("DB connected successfully");
-    return server.listen({hostname: "0.0.0.0", port: process.env.PORT || 8000});
-}).then(({url}) => {
-    console.log(`🚀 Server running at ${url}`);
-});
+    // Set up our Express middleware to handle CORS, body parsing,
+    // and our expressMiddleware function.
+    app.use(
+    '/',
+    cors(),
+    bodyParser.json(),
+    // expressMiddleware accepts the same arguments:
+    // an Apollo Server instance and optional configuration options
+    expressMiddleware(server, {
+        context: async ({ req }) => ({ token: req.headers.token }),
+    }),
+    );
+
+    // Modified server startup
+    await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+
+    console.log(`🚀 Server ready at http://localhost:4000/`);
+}
+
+startApolloServer();
